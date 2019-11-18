83255785d0056d30680974c653c53efcf4d73d68
wonderfulnx <naxinlegend@outlook.com>
2019-11-18 16:54:14 +0800

    tested many method, KMeans, Birch, SpectralClustering, Agglomerative Clustering all have pretty good results

--- P2/sklearn_test.py
+++ P2/sklearn_test.py
@@ -7,6 +7,10 @@ import numpy as np
 from sklearn import metrics
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
+from sklearn.cluster import Birch
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.cluster import AgglomerativeClustering
 
 def read_data():
     data_x = []
@@ -24,18 +28,49 @@ if __name__ == "__main__":
     data_x = np.array(data)
 
     # KMeans
-    # kmeas_model = KMeans(n_clusters=4, random_state=1).fit(data_x)
+    # kmeas_model = KMeans(n_clusters=5, random_state=1).fit(data_x)
     # labels = kmeas_model.labels_
     # print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
     # print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
 
     # DBSCAN
-    db = DBSCAN(eps=8, min_samples=20).fit(data_x)
-    labels = db.labels_
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
+    # db = DBSCAN(eps=8, min_samples=10).fit(data_x)
+    # labels = db.labels_
+    # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    # n_noise_ = list(labels).count(-1)
+    # print('Estimated number of clusters: %d' % n_clusters_)
+    # print('Estimated number of noise points: %d' % n_noise_)
+    # print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
+    # print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
 
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
-    print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
-    print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
+    # Birch
+    # birch = Birch(n_clusters=4).fit(data_x)
+    # labels = birch.labels_
+    # print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
+    # print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
+
+    # SpectralClustering gamma: 0.01 -> 5
+    # spec = SpectralClustering(n_clusters=5, gamma=0.03).fit(data_x)
+    # labels = spec.labels_
+    # print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
+    # print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
+
+    # MeanShift
+    # bandwidth = estimate_bandwidth(data_x, quantile=0.16, n_samples=None)
+    # ms = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(data_x)
+    # labels = ms.labels_
+    # labels_unique = np.unique(labels)
+    # n_clusters_ = len(labels_unique)
+    # print('Estimated number of clusters: %d' % n_clusters_)
+    # print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
+    # print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
+
+    # Agglomerative Clustering
+    for linkage in ('ward', 'average', 'complete', 'single'):
+        clustering = AgglomerativeClustering(linkage=linkage, n_clusters=5)
+        agg = clustering.fit(data_x)
+        labels = agg.labels_
+        print(linkage)
+        print('Calinski-Harabasz:', metrics.calinski_harabasz_score(data_x, labels))
+        print('Silhouette Coefficient:', metrics.silhouette_score(data_x, labels, metric='euclidean'))
+        print()
